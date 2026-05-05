679cae0aee4455c82b1775c9fb42c8fe6696aa52
Noeng soknit <noengsoknoeth22@gmail.com>
2026-05-06 00:09:07 +0700

    Add files via upload

---
+++ New folder/include iostream.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;// cout, cin 
+int main()
+{
+    int age=10;
+    cout<<"Hello";
+    cout"age"20;
+    return 0;
+} 
---
+++ Untitled1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+in main(ahh) {
+	int arr[5] = {5, 3, 1, 4, 2,};
+	//
+	cout<<"show: ";
+	for (int i = 0; i <5; i++) {
+		cout <<arr[i]<<"";
+	}
+	cout <<endl;
+	// number reab
+	for (int i = 0; i <5; i++) {
+		 if (arr{i})> arr[j]{
+		 	int temp = arr[i];
+		 	arr [i] = arr [i;]
+		 	arr [j] = temp;
+	  }
+	} 
+}
+// show the sem
+cout <<"ort dg: ";
+for (int i = 0; i < 5; i++)	{
+	cout << arr [i] << " ";
+}
+cout << endl;
+return 0;
+}
